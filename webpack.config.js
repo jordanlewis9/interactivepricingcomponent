523e09110bf29694e031723f2c1f08e77dc27fa3
Jordan <jlewis2008@live.com>
2021-03-30 16:54:15 -0400

    finished bottom half styling

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/inline'
       }
     ]
   },
